9934a99593db7e47c791e1b3fa79b74bad98f4a3
adhiman1999 <abhishek.d@freestand.in>
2022-01-20 22:27:54 +0530

    latest

--- backend/config/winston.js
+++ backend/config/winston.js
@@ -6,7 +6,7 @@ var winston = require('winston');
 var options = {
   file: {
     level: 'info',
-    filename: '/var/log/app.log',
+    filename: `${appRoot}/log/app.log`,
     handleExceptions: true,
     json: true,
     maxsize: 5242880, // 5MB
